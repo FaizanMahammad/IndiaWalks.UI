62324be35d8d678c665a9e682ce433edba5c88e5
FaizanMahammad <148524354+FaizanMahammad@users.noreply.github.com>
2026-03-31 22:19:06 +0530

    Create AddRegionViewModel.cs

---
+++ Models/AddRegionViewModel.cs
@@ -0,0 +1,10 @@
+namespace IndiaWalks.UI.Models
+{
+    public class AddRegionViewModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+
+    }
+}
